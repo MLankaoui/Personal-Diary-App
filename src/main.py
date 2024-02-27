21588d3340b17592f5d823d1ec031cac196eb728
MLankaoui <marouanelankaoui66@gmail.com>
2024-02-27 10:39:30 +0100

    main function defined and added some line of code to it with the definition of greetings function

---
+++ src/main.py
@@ -0,0 +1,15 @@
+def main():
+    print("welcome to the personal diary app")
+
+    print("============================")
+
+    name = input("please enter your name : ")
+
+    greetings(name)
+
+
+
+def greetings(name):
+    print(f"hello mr {name} and welcome!")
+
+main()
