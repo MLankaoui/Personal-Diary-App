64bb153989785e518bba843291dcf5dceafd3866
MLankaoui <marouanelankaoui66@gmail.com>
2024-02-27 10:53:35 +0100

    more code in main.py

--- src/main.py
+++ src/main.py
@@ -7,9 +7,36 @@ def main():
 
     greetings(name)
 
+    options(name)
 
+    user_option = options(name)
 
+    if (user_option == 1):
+        pass #just for now
+    elif (user_option == 2):
+        pass
+
+    elif (user_option == 3):
+        pass
+
+    elif (user_option == 4):
+        pass
+
+    else:
+        print('not a valid option')
 def greetings(name):
     print(f"hello mr {name} and welcome!")
+
+
+def options(name):
+    print(f"okey then mr {name} please choose between these options")
+
+    print("""
+1) writing a new diary.
+2) deleting a diary
+3) reviewing a diary
+4) editing a diaryss""")
+    option = int(input("> your option here : "))
+    return (option)
 
 main()
